568d6e3e160caf35bf5e313446008fdcd6a0da10
Ansh Kamkhalia <ansh@Anshs-MacBook-Air.local>
2026-01-08 22:09:23 -0500

    create first ball tracking model, need to improve

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # todo
 
-combine slices and volleys into one class
+combine slices and volleys into one class and add serves
+
+do ball tracking in the meanwhile
+
+for ball tracking:
+- improve accuracy (flickering)
+- try using a custom cnn with augmented copies
---
+++ src/ball_tracking/tester.py
@@ -0,0 +1,19 @@
+# tests the fine-tuned yolo model on actual videos
+
+from ultralytics import YOLO
+
+i = 4 # video" index to predict on
+video_path = f"data/court-level-videos/videoplayback{i}.mp4"
+
+# load trained yolo instance
+model = YOLO("runs/detect/train2/weights/best.pt")
+
+# Run prediction on a video
+results = model.track(
+    source=video_path,   # input video
+    conf=0.20,           # confidence threshold
+    save=True,           # save output
+    project="outputs",   # change output directory
+)
+
+# results contains per-frame predictions
